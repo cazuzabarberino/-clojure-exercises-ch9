27002668318be3f57460bb61a2c39841201f9009
cazuzaneves <cazuzaneves@gmail.com>
2020-07-23 16:45:25 -0300

    end

--- src/clojure_exercices_ch9/core.clj
+++ src/clojure_exercices_ch9/core.clj
@@ -6,3 +6,42 @@
   [& args]
   (println "Hello, World!"))
 
+(defmacro wait
+  "Sleep `timeout` seconds before evaluating body"
+  [timeout & body]
+  `(do (Thread/sleep ~timeout) ~@body))
+
+(let [saying3 (promise)]
+  (future (deliver saying3 (wait 100 "Cheerio!")))
+  @(let [saying2 (promise)]
+     (future (deliver saying2 (wait 400 "Pip pip!")))
+     @(let [saying1 (promise)]
+        (future (deliver saying1 (wait 200 "'Ello, gov'na!")))
+        (println @saying1)
+        saying1)
+     (println @saying2)
+     saying2)
+  (println @saying3)
+  saying3)
+
+
+(defn p-race
+  [values]
+  (let [p (promise)]
+    (doseq [v values]
+      (future (wait (* v 1000) (deliver p v))))
+    p))
+
+(def p (p-race [3 2 5 7]))
+(println @p)
+
+
+(defn p-all
+  [values]
+  (let [promises (map
+                  (fn [v] (future (wait (* v 1000) v)))
+                  values)]
+    (future (map deref promises))))
+
+(def p-data (p-all [3 2 2 1]))
+(println @p-data)
